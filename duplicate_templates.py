1dd914132d16a94cc1e370c0febe4177082fdcca
gffbss <gboss4@gmail.c>
2015-08-03 14:43:54 -0700

    updated and cleaned up core logic for duplication

--- duplicate_templates.py
+++ duplicate_templates.py
@@ -4,71 +4,62 @@ from selenium.webdriver.common.by import By
 import csv
 
 # TODO: Handle error handling for password input
-def switch_tabs(driver):
+def _switch_tabs(driver):
 	for handle in driver.window_handles:
 	    driver.switch_to_window(handle)
 
-def duplicate_template_for_student():
-	driver = webdriver.Firefox()
-	driver.get("https://app.asana.com/0/21372080955173/21372080955173")
+
+def _check_completion_status():
+	finished = raw_input('Have the projects finished duplicating? (Y/N): ')	
+	if finished.lower() == 'y':	
+		driver.close()
+	else:
+		finished = raw_input('Have the projects finished duplicating? (Y/N): ')
+
+
+def _duplicate_template_for_student():
+	# This should be the url of the opened project for duplication
+	get_url = raw_input('Please provide the url where the projects will live: ')
+	driver = webdriver.Firefox()	
+	driver.get(get_url)
 	driver.find_element_by_id('google_auth_button').click()
 
-	switch_tabs(driver)	
+	_switch_tabs(driver)	
 
-	username = driver.find_element_by_name('Email')
-	passwd = driver.find_element_by_name('Passwd')
+	username = driver.find_element_by_name('Email')	
 
 	username.send_keys('geoff.boss@hackreactor.com')	
-	get_passwd = raw_input('Please provide your password: ')
+	driver.find_element_by_id('next').click()
 
+	get_passwd = raw_input('Please provide your password: ')
+	passwd = driver.find_element_by_name('Passwd')
 	passwd.send_keys(get_passwd)
 	driver.find_element_by_id('signIn').click()
 	print 'we are logging in'
 
-	switch_tabs(driver)		
+	_switch_tabs(driver)		
 
-	with open('testing_names.csv') as csvfile:
+	with open('testing_new_names.csv') as csvfile:
 		print 'in the open'
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			if len(row['Name']) > 5:
-				driver.implicitly_wait(5)
+			if len(row['Name']) > 5:				
+				driver.implicitly_wait(40)
 				driver.find_element_by_id('pot_action_menu').click()
 				driver.find_element_by_id('duplicate_project').click() 
 				student_name_field = driver.find_element_by_id('duplicate_object_name_input')
 				student_name_field.send_keys(row['Name'])
 				driver.find_element_by_id('duplicate_object_dialog_submit').click()
 				print 'creating user: ' + row['Name']
-				driver.implicitly_wait(12)						
+				driver.implicitly_wait(40)										
+	
+	driver.implicitly_wait(60)
+	_check_completion_status()
+	
+if __name__ == '__main__':
+	try:
+		_duplicate_template_for_student()	
+	except Exception, e:
+		print e	
 
-	print 'we hit the end of the loop'
-	driver.implicitly_wait(15)
-	driver.close()
-
-
-duplicate_template_for_student()	
-# new_student_name = raw_input('What is the students name: ')
-
-# student_name_field.send_keys(new_student_name)
-# driver.find_element_by_id('duplicate_object_dialog_submit').click()
-
-# new_user = driver.find_element_by_xpath('//span[contains(.,' + '"' + new_student_name + '"' + ')]')
-# new_user = driver.find_elements_by_xpath("//span[contains(text(), " + "'" + new_student_name + "'" + ")]")
-# new_user = driver.find_element(By.XPATH, "//span[contains(text(), " + "'" + new_student_name + "'" + ")]")
-
-# driver.implicitly_wait(15)
-# for i in new_user:
-# 	print i
-
-# new_user.click()
-# print 'we clickin'
-# driver.find_element_by_id('project_members_button').click()
-# driver.find_element_by_id('project_members_button/add_user').click()
-
-# new_student_email = raw_input('Enter email address:' )
-# driver.find_element_by_id('project_members_button/add_user').send_keys(new_student_email)
-
-# add_new_user = driver.find_elements_by_xpath("//div[contains(text(), 'Add Member')]")
-# # add_new_user = driver.find_element_by_xpath('//div[contains(.,'Add Member')]')
-# add_new_user.click()
 
