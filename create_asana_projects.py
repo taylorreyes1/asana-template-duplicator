cee56d3df52955f9c0c644c8b022ea2ffc94496c
gffbss <gboss4@gmail.c>
2016-02-24 14:14:13 -0800

    Moved instructions to seperate file

--- create_asana_projects.py
+++ create_asana_projects.py
@@ -1,8 +1,9 @@
 import asana
 import csv
-from dev import private_access_token, followers, instructions, hired, scheduling
+from instructions import followers, instructions, hired, scheduling
+from dev import private_access_token
 
-TASKS = ['Graveyard (**ONLY IF YOU REJECT COMPANY"" ELSE MARK COMPLETE):', 'Leads:', 'Applied:', 'Phone Screen Scheduled:', 'Phone Screen Completed:', 'Technical Screen Scheduled:', 'Technical Screen Completed:', 'Coding Challenge Recieved:', 'Coding Challenge Completed:', 'On-Site Interview Scheduled:', 'On-Site Interview Completed:', 'Offers Recieved:', 'Scheduling Technical Interview Practice Sessions', 'Getting Started on Hired.com', '$$Instructions - Do Not Modify or Delete', 'Working Area (NOT Companies):',]
+TASKS = ['Graveyard (**ONLY IF YOU REJECT COMPANY"" ELSE MARK COMPLETE):', 'Leads:', 'Applied:', 'Phone Screen Scheduled:', 'Phone Screen Completed:', 'Technical Screen Scheduled:', 'Technical Screen Completed:', 'Coding Challenge Recieved:', 'Coding Challenge Completed:', 'On-Site Interview Scheduled:', 'On-Site Interview Completed:', 'Offers Received:', 'Scheduling Technical Interview Practice Sessions', 'Getting Started on Hired.com', '$$Instructions - Do Not Modify or Delete', 'Working Area (NOT Companies):',]
 
 client = asana.Client.access_token(private_access_token)
 me = client.users.me()
@@ -13,16 +14,16 @@ reactor_workspace_id = me['workspaces'][0]['id']
 # Grab the team names
 teams = client.teams.find_by_organization(reactor_workspace_id)
 # This must reference the team you'd like the projects in
-team = next(team for team in teams if team['name'] == 'Testing tools')
+team = next(team for team in teams if team['name'] == 'HR - FEB 2016 JOB SEEKERS')
 
-file_to_open = 'testing_new_names.csv'
+file_to_open = 'HR36-37-Updated-Asana.csv'
 with open(file_to_open) as csvfile:
 	print 'in the open'
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		if len(row['Name']) > 5:
 			create_project = client.projects.create_in_workspace(reactor_workspace_id, {'team': team['id'],  'name': row['Name']})			
-			# client.projects.add_followers(create_project['id'], {'followers': followers})
+			client.projects.add_followers(create_project['id'], {'followers': followers})
 			
 			print "Created project with id: " + str(create_project['id'])
 			for i in TASKS:				
---
+++ instructions.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# Modify the text of this file such that it meets your requirements
+followers = ['geoff.boss@hackreactor.com', 'zaina.natour@hackreactor.com', 'marlene.tang@hackreactor.com', 'margot.weber@hackreactor.com', 'michelle.miller@hackreactor.com', 'ruan@hackreactor.com', 'nick@hackreactor.com', 'caroline@hackreactor.com', 'erin.freeny@hackreactor.com', 'tara.mclauchlan@hackreactor.com', 'shawn@hackreactor.com', 'pamela.greenberg@hackreactor.com', 'stephen.wu@hackreactor.com', 'jira.vinyoopongphan@hackreactor.com', 'justin.mancherje@hackreactor.com']
+# instructions
+instructions = """ If you have an urgent concern, call your respective Job Search Mentor, either Marlene (408-839-0360), Margot (610-585-7688), Geoff (818-903-1913) or Zaina (434-221-6133).
+
+1. NEW COMPANIES
+Add new companies as leads. Use description area of the task for notes about the company/position and links to job descriptions.
+
+2. TRACKING PROGRESS
+As you move through stages with a company, move the company into the appropriate section. Make sure to distinguish between the "Scheduled" and "Completed" sections for interviews and challenges.
+
+Please update progress at least daily in the comments section of each company's task.  
+
+3. CONCLUDED CONVERSATIONS
+If a company turns you down, check them off as complete and leave them in the section of your Asana that corresponds to how far you got in the interview process. Should you decide not to move forward with a company, move them into the graveyard section. 
+
+In both cases, it is useful to record notes that could allow us to help diagnose problems and better target your search.
+
+4. OFFERS
+When you get an offer, please upload any details or written offers to your corresponding asana project and e-mail outcomes@hackreactor.com and let the team know the terms of your offer. Do not accept, decline, negotiate or make any commitments prior to notifying the Student Outcomes Team.
+
+5. QUESTIONS AND COMMUNICATIONS
+Questions regarding hiring partners, connections with alums at companies, and about offers received should be directed to outcomes@hackreactor.com during the course of your search. Questions regarding the mechanics of your job search, issues you are having during or preparing for interviews, or general questions or concerns regarding your job search should also be directed to outcomes@hackreactor.com. """
+
+
+hired = """ ** Note we cannot guarantee acceptance ** 
+
+-- [STEP 1] --
+Go though this link: https://hired.com/?utm_source=partner&utm_medium=hackreactor
+
+Step 1:
+In which cities would you like to work?
+How many years of work experience do you have in Software Engineering?
+-  Do not put 0-1 - you will have to take the coding challenge. It's okay to put 1-2.
+How many years of experience do you have in each of the following?
+- Put 1-2 for this too -- this is the level at which you will be able to contribute.
+What type of employment are you seeking?
+
+Step 2:
+Upload your social profiles (linkedin, github, etc)
+
+Step 3:
+Import your resume
+** Hired will likely mess this up and put your projects as work experience so be sure to fix that **
+
+Step 4: (Last Steps)
+Where are you in your job search?
+If you found a great fit, when would you want to start your next job?
+When is the earliest date you would like your profile to be visible to employers?
+What base salary are you looking for in your next role? (This is the one place you should put a number, I usually suggest 105 or your comfortable base line)
+Why are you interested in new job opportunities?
+- Keep it short and don't think about it too hard.
+Phone
+
+'Request Invite'
+
+After hitting 'request invite', this will take you to your profile page.
+
+-- [STEP 2] --
+Fill out the summary. The summary is perhaps the most critical thing for your hired profile. After you create your draft, please post in in the comments so that we can review it before you do the final submit!! 
+
+DO NOT HIT THE FINAL SUBMIT BUTTON UNTIL WE HAVE APPROVED YOUR SUMMARY.
+
+Note: Be sure to put Hack Reactor under your education section.
+
+Summary skeleton:
+## About Me
+example:
+"I am a full-stack JavaScript engineer with extensive experience building and architecting production applications in various of environments, frameworks and languages. My passion is to engineer usable, beautiful and scalable software."
+
+## Recent Experience:
+example:
+"Most recently I've been consulting with a three person startup focused on creating a next generation incentive based sales compensation platform which solves the technical customization complexities incumbent products have been unable to solve for decades. With nothing more than crude wireframes and preliminary backend architecture, I drove the full-stack development of the product to beta stage completion in just under a month. The technologies used include Node.js, Restify, PostgreSQL, Docker and Ansible on the back-end, and React with Flux on the front-end."
+
+example:
+"Previously, I was engineering for a corporate training company called Xcelus. My coworker and I noticed some serious scaling issues with the company's previous implementation of their core product. We pitched and implemented a new cloud SaaS platform to the CEO that was scalable for the clients they were bringing on and that also saved $5,000 - $10,000 per client. The old platform was built in PHP and was a custom install for each client; which left the company (and me) managing over 50 databases. We engineered the platform usingJava Play Framework while leveraging AWS for deployment."
+
+##What I’m Looking For Next
+example:
+Write what you a looking for in a potential employer, what kind of space you are interested in etc. """
+
+scheduling = """ To schedule go through this link:
+https://www.google.com/calendar/selfsched?sstoken=UUFmR09HMlZoQVlXfGRlZmF1bHR8MzBkODA3NWFkNmM3MTM2NGNmMWE3ZjkzNTcyZmM4YTQ """
